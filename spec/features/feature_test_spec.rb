c848905662ac0e29e06eca34755fb66696fd1ab9
Alex Avlonitis <avlonitis@msn.com>
2016-02-09 18:22:31 +0000

    added links view

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -2,8 +2,9 @@ require_relative "../spec_helper"
 
 feature 'viewing links' do
   scenario 'when a user visits the homepage it displays links' do
-    Link.create(:title => "title", :url => "url")
+    Link.create(:title => "My Site", :url => "alex.avlonitis.me")
     visit('/links')
-    expect(page).to have_content("title")
+    expect(page).to have_content("My Site")
+    expect(page).to have_content("alex.avlonitis.me")
   end
 end
