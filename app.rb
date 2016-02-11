f14e27f87e4e08dc0a7c98610928088e2f35a81a
Eilw <eirikwiig@gmail.com>
2016-02-11 10:18:59 +0000

    finished 18

--- app.rb
+++ app.rb
@@ -45,8 +45,6 @@ class Bookmarks < Sinatra::Base
 
   post '/signup' do
     user = User.create(name: params[:name], email: params[:email], password: params[:password])
-    user
-    user.id
     session[:user_id] = user.id
     redirect('/links')
   end
