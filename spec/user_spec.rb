abc5837a549f8cc8027ded2b8b61909a309691de
Eilw <eirikwiig@gmail.com>
2016-02-12 12:23:06 +0000

    added a negative test for authenticate

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,15 @@
 require_relative '../app/models/user'
 
 describe User do
-  subject(:user){ described_class.new }
+  let(:user){User.create(name: 'Alex', email:'Alex@Alex.com', password: '123', password_confirmation: '123')}
 
   describe '#authenticate' do
     it 'returns the user_id if authenticated' do
-      alex_user = User.create(name: 'Alex', email:'Alex@Alex.com', password: '123', password_confirmation: '123')
-      expect(alex_user.authenticate(123)).to eq alex_user.id
+      expect(user.authenticate(123)).to eq user.id
+    end
+
+    it 'returns nil if incorrect information provided' do
+      expect(user.authenticate(12345)).to be nil
     end
   end
 end
